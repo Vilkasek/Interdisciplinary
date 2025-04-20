fe036f9b62400a6735dd52acfdce986ea4658100
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-04-20 16:53:17 +0200

    Added main game class and simple window

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
-def main() -> None:
-    print("Hello world")
+import sys
+
+import pygame
 
 
-main()
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.WIDTH, self.HEIGHT = 1280, 720
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Hydro Mazury")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            self.clock.tick(60)
+
+        self.quit()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((30, 30, 30))
+        pygame.display.flip()
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
