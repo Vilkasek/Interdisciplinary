ca6fdadf01ef16548c16a500a3e15d3bdb0bea2a
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-04-21 10:19:58 +0200

    Added else section to checking state existing

--- utils/states.py
+++ utils/states.py
@@ -6,4 +6,7 @@ class State:
     def change_state(cls, state: str) -> None:
         if state in cls.states:
             cls.state = state
-        # TODO: Add error handling
+        else:
+            raise ValueError(
+                f"State error: {state} don't exist. Correct states: {cls.states}"
+            )
