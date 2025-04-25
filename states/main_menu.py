7322edd0ae71c94063e644056f81b168e445264c
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-04-25 21:20:35 +0200

    Added water level button to main menu

--- states/main_menu.py
+++ states/main_menu.py
@@ -6,13 +6,19 @@ from utils.palette import *
 
 class MainMenu:
     def __init__(self) -> None:
+        self.font = pygame.font.Font("assets/fonts/Helvetica.ttf", 32)
+
         self.logo_surface = pygame.image.load("assets/graphics/logo.png")
         self.logo_rectangle = self.logo_surface.get_rect(topleft=(20, 20))
 
         self.title_surface = pygame.image.load("assets/graphics/title.png")
         self.title_rectangle = self.title_surface.get_rect(topleft=(300, 20))
 
-        self.polution_button = Button("assets/graphics/water_level.png", (20, 100))
+        self.water_button = Button("assets/graphics/water_level.png", (20, 220))
+        self.water_text_surface = self.font.render(
+            "Poziom wód", True, blue_text_color, None
+        )
+        self.water_text_rectangle = self.water_text_surface.get_rect(topleft=(220, 300))
 
     def render(self, w: pygame.Surface):
         self.logo_surface = self.logo_surface.convert_alpha()
@@ -20,3 +26,7 @@ class MainMenu:
 
         w.blit(self.logo_surface, self.logo_rectangle)
         w.blit(self.title_surface, self.title_rectangle)
+
+        w.blit(self.water_text_surface, self.water_text_rectangle)
+
+        self.water_button.render(w)
--- utils/palette.py
+++ utils/palette.py
@@ -1,3 +1,6 @@
 background_color: tuple = (224, 224, 224)
+
 dark_text_color: tuple = (0, 0, 0)
 light_text_color: tuple = (255, 255, 255)
+
+blue_text_color: tuple = (0, 84, 148)
