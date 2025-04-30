3d6992b191ffbb43cfa288ae1028c403681d75fc
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-04-30 08:27:08 +0200

    Added colors to palette

--- utils/palette.py
+++ utils/palette.py
@@ -4,3 +4,5 @@ dark_text_color: tuple = (0, 0, 0)
 light_text_color: tuple = (255, 255, 255)
 
 blue_text_color: tuple = (0, 84, 148)
+orange_text_color: tuple = (254, 141, 40)
+green_text_color: tuple = (0, 145, 0)
