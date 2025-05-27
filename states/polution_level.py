1e9dfa857d77b98c7008933b8b3f271f8e288209
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-05-27 13:51:12 +0200

    Dodałem do innych stanów wyświetlanie gui

--- states/polution_level.py
+++ states/polution_level.py
@@ -1,12 +1,14 @@
 import pygame
 
+from utils.ui import UI
+
 
 class Polution:
     def __init__(self) -> None:
-        pass
+        self.ui = UI()
 
     def handle_events(self, ev: pygame.event.Event):
-        pass
+        self.ui.handle_events(ev)
 
     def render(self, w: pygame.surface.Surface):
-        pass
+        self.ui.render(w)
