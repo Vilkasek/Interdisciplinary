f7294bed7f9ef5f329b4324a5fc98b26bdb0d761
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-05-27 13:01:30 +0200

    Wyeksportowałem kod związany z działaniem interfaceu do innej klasy, aby móc go ponownie łatwo używać

--- states/main_menu.py
+++ states/main_menu.py
@@ -1,56 +1,13 @@
 import pygame
 
-from utils.button import Button
-from utils.palette import *
-from utils.states import State
-
+from utils.ui import UI
 
 class MainMenu:
     def __init__(self) -> None:
-        self.font = pygame.font.Font("assets/fonts/Helvetica.ttf", 32)
-
-        self.logo_surface = pygame.image.load("assets/graphics/logo.png")
-        self.logo_rectangle = self.logo_surface.get_rect(topleft=(20, 20))
-
-        self.title_surface = pygame.image.load("assets/graphics/title.png")
-        self.title_rectangle = self.title_surface.get_rect(topleft=(300, 20))
-
-        self.buttons = [
-            Button("assets/graphics/water_level.png", (20, 220)),
-            Button("assets/graphics/polution_level.png", (20, 418)),
-            Button("assets/graphics/temperature_level.png", (20, 619)),
-            Button("assets/graphics/generate_report.png", (20, 750)),
-        ]
-
-        self.water_text_surface = self.font.render(
-            "Poziom wód", True, blue_text_color, None
-        )
-        self.water_text_rectangle = self.water_text_surface.get_rect(topleft=(220, 300))
-
-        self.report_text_surface = self.font.render(
-            "Generuj raport", True, blue_text_color, None
-        )
-        self.report_text_rectangle = self.report_text_surface.get_rect(
-            topleft=(220, 750)
-        )
-
-        self.state = State()
+        self.ui = UI()
 
     def handle_events(self, ev: pygame.event.Event):
-        if self.buttons[0].is_clicked(ev):
-            self.state.change_state("WATER")
-        elif self.buttons[3].is_clicked(ev):
-            self.state.change_state("GENERATE_REPORT")
+       self.ui.handle_events(ev)
 
     def render(self, w: pygame.Surface):
-        self.logo_surface = self.logo_surface.convert_alpha()
-        self.title_surface = self.title_surface.convert_alpha()
-
-        w.blit(self.logo_surface, self.logo_rectangle)
-        w.blit(self.title_surface, self.title_rectangle)
-
-        w.blit(self.water_text_surface, self.water_text_rectangle)
-        w.blit(self.report_text_surface, self.report_text_rectangle)
-
-        for button in self.buttons:
-            button.render(w)
+        self.ui.render(w)
--- utils/ui.py
+++ utils/ui.py
@@ -1,3 +1,45 @@
+import pygame
+
+from utils.button import Button
+from utils.palette import *
+from utils.states import State
+
+
 class UI:
     def __init__(self):
-        pass
+        self.font = pygame.font.Font("assets/fonts/Helvetica.ttf", 32)
+
+        self.buttons = [
+            Button("assets/graphics/water_level.png", (20, 220)),
+            Button("assets/graphics/polution_level.png", (20, 418)),
+            Button("assets/graphics/temperature_level.png", (20, 619)),
+        ]
+
+        self.water_text_surface = self.font.render(
+            "Poziom wód", True, blue_text_color, None
+        )
+        self.water_text_rectangle = self.water_text_surface.get_rect(topleft=(220, 300))
+
+        self.logo_surface = pygame.image.load("assets/graphics/logo.png")
+        self.logo_rectangle = self.logo_surface.get_rect(topleft=(20, 20))
+
+        self.title_surface = pygame.image.load("assets/graphics/title.png")
+        self.title_rectangle = self.title_surface.get_rect(topleft=(300, 20))
+
+        self.state = State()
+
+    def handle_events(self, ev: pygame.event.Event):
+        if self.buttons[0].is_clicked(ev):
+            self.state.change_state("WATER")
+
+    def render(self, w: pygame.Surface):
+        self.logo_surface = self.logo_surface.convert_alpha()
+        self.title_surface = self.title_surface.convert_alpha()
+
+        w.blit(self.logo_surface, self.logo_rectangle)
+        w.blit(self.title_surface, self.title_rectangle)
+
+        w.blit(self.water_text_surface, self.water_text_rectangle)
+
+        for button in self.buttons:
+            button.render(w)
