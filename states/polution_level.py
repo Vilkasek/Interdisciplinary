cfa2ac5719588258de48f95cb2d973e2b34b2a0a
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-05-28 15:39:34 +0200

    Dodałem przyciski generowania raportów do odpowiednich klas

--- states/polution_level.py
+++ states/polution_level.py
@@ -1,14 +1,17 @@
 import pygame
 
+from utils.button import Button
 from utils.ui import UI
 
 
 class Polution:
     def __init__(self) -> None:
+        self.generate_button = Button("assets/graphics/polution_report.png", (800, 750))
         self.ui = UI()
 
     def handle_events(self, ev: pygame.event.Event):
         self.ui.handle_events(ev)
 
     def render(self, w: pygame.surface.Surface):
+        self.generate_button.render(w)
         self.ui.render(w)
--- states/temperature_level.py
+++ states/temperature_level.py
@@ -1,14 +1,17 @@
 import pygame
 
+from utils.button import Button
 from utils.ui import UI
 
 
 class Temperature:
     def __init__(self) -> None:
+        self.generate_button = Button("assets/graphics/temperature_report.png", (800, 750))
         self.ui = UI()
 
     def handle_events(self, ev: pygame.event.Event):
         self.ui.handle_events(ev)
 
     def render(self, w: pygame.surface.Surface):
+        self.generate_button.render(w)
         self.ui.render(w)
