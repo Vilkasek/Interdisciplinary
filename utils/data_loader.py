a3dfe6f0a1f69a8602ae6f115de5b426af5e6810
Vilkasek <109431561+Vilkasek@users.noreply.github.com>
2025-05-28 16:20:24 +0200

    Dodałem TODOs, aby nie zapomnieć co miałem dodać

---
+++ utils/data_loader.py
@@ -0,0 +1,1 @@
+# TODO: Dodać kod do ładowania danych z plików JSON, do których będą podawane ścieżki
---
+++ utils/report_generation.py
@@ -0,0 +1,1 @@
+# TODO: Dodać generowanie pliku PDF, jako raport z danych, które zwraca plik data_loader.py
